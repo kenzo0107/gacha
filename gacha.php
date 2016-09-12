8fb6b2e16735d59b266faa4d4d82d9bb0f0b379a
田中賢三 <kenzo.tanaka0107@gmail.com>
2016-09-12 22:53:13 +0900

    Update: rand でなく mt_rand で偏りを微修正
    ガチャの精度にrand では偏りが強かったため、mt_rand へ修正

--- gacha.php
+++ gacha.php
@@ -3,18 +3,18 @@
 $gacha_list = array(
     '1' => array( 'name' => 'Item1', 'rate' => 30 )
    ,'2' => array( 'name' => 'Item2', 'rate' => 35 )
-   ,'3' => array( 'name' => 'Item3', 'rate' => 1 )
+   ,'3' => array( 'name' => 'Item3', 'rate' => 20 )
 );
 
 $total_rate = 0;
 foreach ( $gacha_list as $id => $v ) {
-    if ( !empty( $v['rate'] ) ) {
+    if ( is_int($v['rate']) && $v['rate'] > 0 ) {
         $total_rate += $v['rate'];
         $list[$id]['range_end'] = $total_rate;
     }
 }
 
-$index = rand( 1, $total_rate );
+$index = mt_rand( 1, $total_rate );
 
 foreach ( $list as $id => $v ) {
     if ( $index <= $v['range_end'] ) {
